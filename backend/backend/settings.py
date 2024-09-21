0022d076541174dca9d2728cd7cfccdb1a1e3550
Aditya <aditya.com>
2024-09-21 20:33:42 +0530

    production url changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,8 +68,9 @@ ROOT_URLCONF = 'backend.urls'
 CORS_ALLOW_ALL_ORIGINS = True 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    
     "https://museum-fornt.onrender.com",
+    "http://localhost:3000",
     # React development server
 ]
 
